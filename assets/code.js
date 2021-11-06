fdbfbc69dd8635bd8261b051577c83d1743d5bb5
Jesus Fernandez <jesus.fdz93@gmail.com>
2021-11-06 10:09:13 -0500

    Avance final

--- assets/code.js
+++ assets/code.js
@@ -1,13 +1,24 @@
-const nombre = document.getElementById('name');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
+
 
 const registrar = (e) => {
+
+    const nombre = document.getElementById('name');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+
+    
     
     if (nombre.value === '' || email.value === '' || password.value === '') {
         alert('Todos los campos son obligatorios');
     } else {
-        console.log({name: nombre.value, email: email.value, password: password.value});
+
+        const usuario = {
+            nombre: nombre.value,
+            email: email.value,
+            password: password.value,
+        };
+        
+        console.log(usuario);
 
         nombre.value = '';
         email.value = '';
